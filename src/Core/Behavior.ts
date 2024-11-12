c7a5a570e0e09541e9c42ab5b32e7041b8b7c253
eliott <eliott.jaquier@gmail.com>
2024-11-12 15:34:03 +0100

    feat: add basic implementation of Behavior.ts and GameObject.ts

---
+++ src/Core/Behavior.ts
@@ -0,0 +1,46 @@
+import { GameObject } from './GameObject';
+
+/**
+ * A behavior is a component that can be attached to a GameObject.
+ */
+export abstract class Behavior {
+    private _attachedOn: GameObject | null = null;
+
+    /**
+     * Like onEnable, but should only be for internal use, so some non-abstact direct behaviors could expose attachedOn to the user.
+     * @param attachedOn The GameObject this behavior is attached to.
+     * @throws Error if the behavior is already attached to a GameObject.
+     */
+    public setup(attachedOn: GameObject): void {
+        if (this._attachedOn !== null) {
+            throw new Error("Behavior is already attached to a GameObject.");
+        }
+        this._attachedOn = attachedOn;
+        this.onEnable();
+    }
+
+    /**
+     * Detach this behavior from the GameObject it is attached to.
+     * @param from - The GameObject to detach this behavior from.
+     * @throws Error if the behavior is not attached to the GameObject.
+     */
+    public detach(from: GameObject): void {
+        if (this._attachedOn !== from) {
+            throw new Error("Behavior is not attached to this GameObject.");
+        }
+        this._attachedOn = null;
+        this.onDisable();
+    }
+
+    protected onEnable(): void {
+        //This is not abstract because it is not required to be implemented.
+    }
+
+    protected onDisable(): void {
+        //This is not abstract because it is not required to be implemented.
+    }
+
+    protected tick(deltaTime: number): void {
+        //This is not abstract because it is not required to be implemented.
+    }
+}
