6851af7adc2f37e74dbb9674f35a0c4bde882a6d
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-11-12 15:35:53 +0100

    feat: add a math Utility class

---
+++ src/Core/MathStructures/Utility.ts
@@ -0,0 +1,22 @@
+namespace Core.MathStructures {
+    /**
+     * A utility class for standard and redundant mathematical operations
+     */
+    class Utility {
+        /**
+         * Convert degrees angles to radians
+         * @param degrees
+         */
+        public static degToRad(degrees: number): number {
+            return degrees * Math.PI / 180;
+        }
+
+        /**
+         * Convert radians angles to degrees
+         * @param radians
+         */
+        public static radToDeg(radians: number): number {
+            return radians * 180 / Math.PI;
+        }
+    }
+}
