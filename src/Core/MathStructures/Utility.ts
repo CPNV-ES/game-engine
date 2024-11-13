5483110d1b6f5529785b50399c28d7027b5dbfd2
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-11-13 08:22:31 +0100

    refactor: remove namespaces -> deprecated

--- src/Core/MathStructures/Utility.ts
+++ src/Core/MathStructures/Utility.ts
@@ -1,22 +1,20 @@
-namespace Core.MathStructures {
+/**
+ * A utility class for standard and redundant mathematical operations
+ */
+export class Utility {
     /**
-     * A utility class for standard and redundant mathematical operations
+     * Convert degrees angles to radians
+     * @param degrees
      */
-    class Utility {
-        /**
-         * Convert degrees angles to radians
-         * @param degrees
-         */
-        public static degToRad(degrees: number): number {
-            return degrees * Math.PI / 180;
-        }
+    public static degToRad(degrees: number): number {
+        return degrees * Math.PI / 180;
+    }
 
-        /**
-         * Convert radians angles to degrees
-         * @param radians
-         */
-        public static radToDeg(radians: number): number {
-            return radians * 180 / Math.PI;
-        }
+    /**
+     * Convert radians angles to degrees
+     * @param radians
+     */
+    public static radToDeg(radians: number): number {
+        return radians * 180 / Math.PI;
     }
 }
