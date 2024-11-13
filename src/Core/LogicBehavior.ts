56b4c21f652663e1ca164bf16f723ff05d48e524
eliott <eliott.jaquier@gmail.com>
2024-11-13 16:29:20 +0100

    refactor: data should be only protected and can update field without emitting so data can be mutable

--- src/Core/LogicBehavior.ts
+++ src/Core/LogicBehavior.ts
@@ -16,24 +16,14 @@ export class LogicBehavior<T> extends Behavior {
     public readonly onDataChanged = new Event<T>();
 
     protected gameObject!: GameObject;
-
-    private _data!: T;
+    protected data!: T;
 
     override setup(attachedOn: GameObject) {
         super.setup(attachedOn);
         this.gameObject = attachedOn;
     }
 
-    public get data(): T {
-        return this._data;
-    }
-
-    protected setDataAndNotify(data: T): void {
-        this.setDataQuietly(data);
-        this.onDataChanged.emit(data);
-    }
-
-    protected setDataQuietly(data: T): void {
-        this._data = data;
+    protected notifyDataChanged(): void {
+        this.onDataChanged.emit(this.data);
     }
 }
