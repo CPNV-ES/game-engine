7010af7f1a67a5cecfa8167aedcc2ee5a6fb21b5
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-11-14 08:00:25 +0100

    test: add tests for MathUtility

---
+++ test/Core/MathStructures/MathUtility.test.ts
@@ -0,0 +1,25 @@
+import {describe, it, expect, vi, Mock} from 'vitest';
+import {MathUtility} from "../../../src/Core/MathStructures/MathUtility";
+
+describe('MathUtility', (): void => {
+
+    /**
+     * Tests if degrees can be successfully converted to radians.
+     */
+    it('should convert degrees to radians', (): void => {
+        const degrees: number = 90;
+        const radians: number = MathUtility.degToRad(degrees);
+
+        expect(radians).toBeCloseTo(Math.PI / 2);
+    });
+
+    /**
+     * Tests if radians can be successfully converted to degrees.
+     */
+    it('should convert radians to degrees', (): void => {
+        const radians: number = Math.PI;
+        const degrees: number = MathUtility.radToDeg(radians);
+
+        expect(degrees).toBeCloseTo(180);
+    });
+});
