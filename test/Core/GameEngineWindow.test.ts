052e8e4641f971e4a7e1fc03cc1a15706574f435
MinusW <pw07vck@eduvaud.ch>
2024-11-19 15:02:32 +0100

    fix(docs): removed singleton dependency and used new for it

--- test/Core/GameEngineWindow.test.ts
+++ test/Core/GameEngineWindow.test.ts
@@ -1,51 +1,45 @@
-import { describe, it, expect } from 'vitest'
+import {describe, it, expect, beforeAll} from 'vitest'
 import { GameEngineComponent } from '../../src/Core/GameEngineComponent'
 import { GameEngineWindow } from '../../src/Core/GameEngineWindow'
+import {GameObject} from "../../src/Core/GameObject";
+import {MockGameEngineComponent} from "./GameEngineComponent.test";
 
 describe("GameEngineWindow", () => {
 
     it("should return a singleton instance", () => {
-        (GameEngineWindow as any)._instance = null;
-
         const instance1: GameEngineWindow = GameEngineWindow.instance;
         const instance2: GameEngineWindow = GameEngineWindow.instance;
 
         expect(instance1).toBe(instance2);
     });
 
-    it("should return the root GameObject", () => {
-        (GameEngineWindow as any)._instance = null;
-
-        const instance: GameEngineWindow = GameEngineWindow.instance;
+    it("should return the root GameObject every time", () => {
+        const instance: GameEngineWindow = new GameEngineWindow;
         const root: GameObject = instance.root;
+
+        expect(root).toBeInstanceOf(GameObject);
     });
 
     it("should add a GameEngineComponent", () => {
-        (GameEngineWindow as any)._instance = null;
-
-        const instance: GameEngineWindow = GameEngineWindow.instance;
-        const component: GameEngineComponent = new GameEngineComponent();
+        const instance: GameEngineWindow = new GameEngineWindow;
+        const component: GameEngineComponent = new MockGameEngineComponent();
 
         instance.addGameComponent(component);
         expect(instance.getEngineComponent(GameEngineComponent)).toBe(component);
     });
 
     it("should retrieve a GameEngineComponent of a specific type", () => {
-        (GameEngineWindow as any)._instance = null;
-
-        const instance: GameEngineWindow = GameEngineWindow.instance;
-        const component: GameEngineComponent = new GameEngineComponent();
+        const instance: GameEngineWindow = new GameEngineWindow;
+        const component: GameEngineComponent = new MockGameEngineComponent();
 
         instance.addGameComponent(component);
-        const retrievedComponent: GameEngineComponent = instance.getEngineComponent(GameEngineComponent);
+        const retrievedComponent: GameEngineComponent = instance.getEngineComponent(MockGameEngineComponent);
 
         expect(retrievedComponent).toBe(component);
     });
 
     it("should return null if a GameEngineComponent of the specified type is not found", () => {
-        (GameEngineWindow as any)._instance = null;
-
-        const instance: GameEngineWindow = GameEngineWindow.instance;
+        const instance: GameEngineWindow = new GameEngineWindow;
         const retrievedComponent: GameEngineComponent = instance.getEngineComponent(GameEngineComponent);
 
         expect(retrievedComponent).toBeNull();
