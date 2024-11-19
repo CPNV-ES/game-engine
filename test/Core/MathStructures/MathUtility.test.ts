de6d6f0f3c080dcd64f5400e6664a73a7d2277aa
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-11-19 13:55:09 +0100

    test: add tests on negative angles convertion

--- test/Core/MathStructures/MathUtility.test.ts
+++ test/Core/MathStructures/MathUtility.test.ts
@@ -22,4 +22,24 @@ describe('MathUtility', (): void => {
 
         expect(degrees).toBeCloseTo(180);
     });
+
+    /**
+     * Tests if negative degrees can be successfully converted to radians.
+     */
+    it('should convert negative degrees to radians', (): void => {
+        const degrees: number = -90;
+        const radians: number = MathUtility.degToRad(degrees);
+
+        expect(radians).toBeCloseTo(-Math.PI / 2);
+    });
+
+    /**
+     * Tests if negative radians can be successfully converted to degrees.
+     */
+    it('should convert negative radians to degrees', (): void => {
+        const radians: number = -Math.PI;
+        const degrees: number = MathUtility.radToDeg(radians);
+
+        expect(degrees).toBeCloseTo(-180);
+    });
 });
