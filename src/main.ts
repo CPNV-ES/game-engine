1c7527b9ce384498daf5e4ee4f67c00af348e07d
eliott.jaquier <eliott.jaquier@eduvaud.ch>
2024-11-27 16:57:55 +0100

    feat: test sprite render behavior

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { GameEngineWindow } from "./Core/GameEngineWindow.ts";
 import { RenderGameEngineComponent } from "./Extensions/RenderEngine/RenderGameEngineComponent.ts";
+import { SpriteRenderBehavior } from "./Extensions/RenderEngine/SpriteRenderBehavior.ts";
 
 const canvas: HTMLCanvasElement =
   document.querySelector<HTMLCanvasElement>("#app")!;
 GameEngineWindow.instance.addGameComponent(
   new RenderGameEngineComponent(canvas, navigator.gpu),
+);
+
+GameEngineWindow.instance.root.addBehavior(
+  new SpriteRenderBehavior(
+    GameEngineWindow.instance.getEngineComponent(RenderGameEngineComponent)!,
+    "src/Test/Sprunk.png",
+  ),
 );
