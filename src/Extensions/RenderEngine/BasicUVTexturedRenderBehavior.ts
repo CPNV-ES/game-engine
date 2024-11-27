041960a0e2d4b13d3fda590c421243fdcc150ea7
eliott.jaquier <eliott.jaquier@eduvaud.ch>
2024-11-27 09:45:15 +0100

    feat: create intermediate render behavior for standard buffer and descriptor

---
+++ src/Extensions/RenderEngine/BasicUVTexturedRenderBehavior.ts
@@ -0,0 +1,45 @@
+import { RenderBehavior } from "./RenderBehavior.ts";
+import { RenderGameEngineComponent } from "./RenderGameEngineComponent.ts";
+
+/**
+ * A RenderBehavior already set up to render a textured object with UV coordinates (GPUBindGroupLayoutDescriptor and GPUVertexBufferLayout are already set up).
+ * BindGroupLayout can be used to set up the bind group layout for the texture and sampler.
+ */
+export abstract class BasicUVTexturedRenderBehavior extends RenderBehavior {
+  protected _bindGroupLayout: GPUBindGroupLayout;
+
+  constructor(
+    renderEngine: RenderGameEngineComponent,
+    vertexWGSLShader: string,
+    fragmentWGSLShader: string,
+  ) {
+    const descriptor: GPUBindGroupLayoutDescriptor = {
+      entries: [
+        { binding: 0, visibility: GPUShaderStage.FRAGMENT, texture: {} },
+        { binding: 1, visibility: GPUShaderStage.FRAGMENT, sampler: {} },
+      ],
+    };
+
+    const buffer: GPUVertexBufferLayout = {
+      arrayStride: 5 * 4, // 3 position floats + 2 UV floats
+      attributes: [
+        { shaderLocation: 0, format: "float32x3", offset: 0 },
+        { shaderLocation: 1, format: "float32x2", offset: 3 * 4 },
+      ],
+    };
+
+    const bindGroupLayout: GPUBindGroupLayout =
+      renderEngine.createBindGroupLayout(descriptor);
+
+    super(
+      renderEngine,
+      vertexWGSLShader,
+      fragmentWGSLShader,
+      "triangle-list",
+      bindGroupLayout,
+      buffer,
+    );
+
+    this._bindGroupLayout = bindGroupLayout;
+  }
+}
