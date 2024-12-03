f9cbc048f44c7c1f1f4bbf0da9bef41fb50a1424
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2024-12-03 14:11:56 +0100

    refactor: abstract (generalize) some bind group and render logic to parent

--- src/Extensions/RenderEngine/BasicUVTexturedRenderBehavior.ts
+++ src/Extensions/RenderEngine/BasicUVTexturedRenderBehavior.ts
@@ -5,9 +5,21 @@ import { RenderGameEngineComponent } from "./RenderGameEngineComponent.ts";
  * A RenderBehavior already set up to render a textured object with UV coordinates (GPUBindGroupLayoutDescriptor and GPUVertexBufferLayout are already set up).
  * BindGroupLayout can be used to set up the bind group layout for the texture and sampler.
  */
-export abstract class BasicUVTexturedRenderBehavior extends RenderBehavior {
+export class BasicUVTexturedRenderBehavior extends RenderBehavior {
+  protected _vertexBuffer: GPUBuffer | null = null;
+  protected _indexBuffer: GPUBuffer | null = null;
+  protected _indexData: Uint16Array;
+  protected _spriteTexture: GPUTexture | null = null;
+  protected _bindGroup: GPUBindGroup | null = null;
+
+  private _spriteImageUrl: RequestInfo | URL;
+  private _vertexData: Float32Array;
+
   constructor(
     renderEngine: RenderGameEngineComponent,
+    spriteImageUrl: RequestInfo | URL,
+    vertexData: Float32Array,
+    indexData: Uint16Array,
     vertexWGSLShader: string,
     fragmentWGSLShader: string,
   ) {
@@ -34,5 +46,49 @@ export abstract class BasicUVTexturedRenderBehavior extends RenderBehavior {
       descriptor,
       buffer,
     );
+
+    this._spriteImageUrl = spriteImageUrl;
+    this._vertexData = vertexData;
+    this._indexData = indexData;
+  }
+
+  protected async asyncInit(): Promise<void> {
+    await super.asyncInit();
+
+    this._vertexBuffer = this._renderEngine.createVertexBuffer(
+      this._vertexData,
+    );
+    this._indexBuffer = this._renderEngine.createIndexBuffer(this._indexData);
+    this._spriteTexture = await this._renderEngine.createTexture(
+      this._spriteImageUrl,
+    );
+    this._bindGroup = this._renderEngine.createBindGroup(
+      this._bindGroupLayout!,
+      [
+        { binding: 0, resource: this._spriteTexture.createView() },
+        {
+          binding: 1,
+          resource: this._renderEngine.createSampler({
+            magFilter: "linear",
+            minFilter: "linear",
+          }),
+        },
+      ],
+    );
+  }
+
+  public render(renderpass: GPURenderPassEncoder) {
+    if (
+      !this._bindGroup ||
+      !this._pipeline ||
+      !this._vertexBuffer ||
+      !this._indexBuffer
+    )
+      return;
+    renderpass.setPipeline(this._pipeline);
+    renderpass.setVertexBuffer(0, this._vertexBuffer);
+    renderpass.setIndexBuffer(this._indexBuffer, "uint16");
+    renderpass.setBindGroup(0, this._bindGroup);
+    renderpass.drawIndexed(this._indexData.length);
   }
 }
--- src/Extensions/RenderEngine/SpriteRenderBehavior.ts
+++ src/Extensions/RenderEngine/SpriteRenderBehavior.ts
@@ -7,94 +7,52 @@ import { BasicUVTexturedRenderBehavior } from "./BasicUVTexturedRenderBehavior.t
  * A UVTextured object, desinged to be rendered with a sprite texture (no repeat, transparency, etc).
  */
 export class SpriteRenderBehavior extends BasicUVTexturedRenderBehavior {
-  protected _spriteTexture: GPUTexture | null = null;
-  protected _bindGroup: GPUBindGroup | null = null;
-  protected _vertexBuffer: GPUBuffer | null = null;
-  protected _indexBuffer: GPUBuffer | null = null;
-
-  private _spriteImageUrl: RequestInfo | URL;
-
-  // Vertex data for a quad (sprite) with UV coordinates and z = 0
-  private _vertexData: Float32Array = new Float32Array([
-    // Position        // UV
-    -0.5,
-    -0.5,
-    0.0,
-    0.0,
-    1.0, // Bottom-left
-    0.5,
-    -0.5,
-    0.0,
-    1.0,
-    1.0, // Bottom-right
-    0.5,
-    0.5,
-    0.0,
-    1.0,
-    0.0, // Top-right
-    -0.5,
-    0.5,
-    0.0,
-    0.0,
-    0.0, // Top-left
-  ]);
-
-  // Index data to form two triangles (quad)
-  protected _indexData: Uint16Array = new Uint16Array([
-    0,
-    1,
-    2, // First triangle
-    2,
-    3,
-    0, // Second triangle
-  ]);
-
   constructor(
     renderEngine: RenderGameEngineComponent,
     spriteImageUrl: RequestInfo | URL,
   ) {
-    super(renderEngine, BasicVertexPassWithUV, BasicTextureSample);
-    this._spriteImageUrl = spriteImageUrl;
-  }
+    // Vertex data for a quad (sprite) with UV coordinates and z = 0
+    const vertexData = new Float32Array([
+      // Position        // UV
+      -0.5,
+      -0.5,
+      0.0,
+      0.0,
+      1.0, // Bottom-left
+      0.5,
+      -0.5,
+      0.0,
+      1.0,
+      1.0, // Bottom-right
+      0.5,
+      0.5,
+      0.0,
+      1.0,
+      0.0, // Top-right
+      -0.5,
+      0.5,
+      0.0,
+      0.0,
+      0.0, // Top-left
+    ]);
 
-  protected async asyncInit(): Promise<void> {
-    await super.asyncInit();
+    // Index data to form two triangles (quad)
+    const indexData = new Uint16Array([
+      0,
+      1,
+      2, // First triangle
+      2,
+      3,
+      0, // Second triangle
+    ]);
 
-    this._vertexBuffer = this._renderEngine.createVertexBuffer(
-      this._vertexData,
+    super(
+      renderEngine,
+      spriteImageUrl,
+      vertexData,
+      indexData,
+      BasicVertexPassWithUV,
+      BasicTextureSample,
     );
-    this._indexBuffer = this._renderEngine.createIndexBuffer(this._indexData);
-    this._spriteTexture = await this._renderEngine.createTexture(
-      this._spriteImageUrl,
-    );
-    this._bindGroup = this._renderEngine.createBindGroup(
-      this._bindGroupLayout!,
-      [
-        { binding: 0, resource: this._spriteTexture.createView() },
-        {
-          binding: 1,
-          resource: this._renderEngine.createSampler({
-            magFilter: "linear",
-            minFilter: "linear",
-          }),
-        },
-      ],
-    );
-  }
-
-  public render(renderpass: GPURenderPassEncoder) {
-    console.log("Rendering sprite");
-    if (
-      !this._bindGroup ||
-      !this._pipeline ||
-      !this._vertexBuffer ||
-      !this._indexBuffer
-    )
-      return;
-    renderpass.setPipeline(this._pipeline);
-    renderpass.setVertexBuffer(0, this._vertexBuffer);
-    renderpass.setIndexBuffer(this._indexBuffer, "uint16");
-    renderpass.setBindGroup(0, this._bindGroup);
-    renderpass.drawIndexed(this._indexData.length);
   }
 }
