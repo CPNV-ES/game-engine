877dcf4cc784c02175595de39c8119c9d9a62f49
MinusW <pw07vck@eduvaud.ch>
2024-12-04 12:15:35 +0100

    fix: fat arrow functions have to be in a variable to be able to remove them

--- src/Extensions/InputSystem/DeviceInputBehavior.ts
+++ src/Extensions/InputSystem/DeviceInputBehavior.ts
@@ -9,53 +9,78 @@ import { Keyboard } from "./Keyboard.ts";
  * @classdesc DeviceInputBehavior is a behavior that listens to input events from devices, it is a helper like an accessor to the devices contained in the InputGameEngineComponent for easier access to the input events.
  */
 export class DeviceInputBehavior extends InputBehavior {
+  // Class-level properties to store observer references
+  private onAnyChangeObserver = () => this.onAnyChange();
+  private onMouseLeftClickUpObserver = () => this.onMouseLeftClickUp();
+  private onMouseLeftClickDownObserver = () => this.onMouseLeftClickDown();
+  private onMouseRightClickUpObserver = () => this.onMouseRightClickUp();
+  private onMouseRightClickDownObserver = () => this.onMouseRightClickDown();
+  private onMouseMoveObserver = (data: MouseEvent) => this.onMouseMove(data);
+  private onMouseScrollObserver = (data: number) => this.onMouseScroll(data);
+
+  private onKeyboardKeyDownObserver = (data: string) =>
+    this.onKeyboardKeyDown(data);
+  private onKeyboardKeyUpObserver = (data: string) =>
+    this.onKeyboardKeyUp(data);
+
+  /**
+   * @description Enables the input events.
+   * Add the observers to the input devices.
+   */
   override onEnable(): void {
     const inputComponent: InputGameEngineComponent | null =
       GameEngineWindow.instance.getEngineComponent(InputGameEngineComponent);
     if (inputComponent === null) return;
+
     const mouse: Mouse | null = inputComponent.getDevice(Mouse);
     if (mouse) {
-      mouse.onAnyChange.addObserver(() => this.onAnyChange());
+      mouse.onAnyChange.addObserver(this.onAnyChangeObserver);
 
-      mouse.onLeftClickUp.addObserver(() => this.onMouseLeftClickUp());
-      mouse.onLeftClickDown.addObserver(() => this.onMouseLeftClickDown());
+      mouse.onLeftClickUp.addObserver(this.onMouseLeftClickUpObserver);
+      mouse.onLeftClickDown.addObserver(this.onMouseLeftClickDownObserver);
 
-      mouse.onRightClickUp.addObserver(() => this.onMouseRightClickUp());
-      mouse.onRightClickDown.addObserver(() => this.onMouseRightClickDown());
+      mouse.onRightClickUp.addObserver(this.onMouseRightClickUpObserver);
+      mouse.onRightClickDown.addObserver(this.onMouseRightClickDownObserver);
 
-      mouse.onMove.addObserver((data) => this.onMouseMove(data));
-      mouse.onScroll.addObserver((data) => this.onMouseScroll(data));
+      mouse.onMove.addObserver(this.onMouseMoveObserver);
+      mouse.onScroll.addObserver(this.onMouseScrollObserver);
     }
     const keyboard: Keyboard | null = inputComponent.getDevice(Keyboard);
     if (keyboard) {
-      keyboard.onAnyChange.addObserver(() => this.onAnyChange());
-      keyboard.onKeyDown.addObserver((data) => this.onKeyboardKeyDown(data));
-      keyboard.onKeyUp.addObserver((data) => this.onKeyboardKeyUp(data));
+      keyboard.onAnyChange.addObserver(this.onAnyChangeObserver);
+      keyboard.onKeyDown.addObserver(this.onKeyboardKeyDownObserver);
+      keyboard.onKeyUp.addObserver(this.onKeyboardKeyUpObserver);
     }
   }
+
+  /**
+   * @description Disables the input events.
+   * Remove the observers from the input devices.
+   */
 
   override onDisable(): void {
     const inputComponent: InputGameEngineComponent | null =
       GameEngineWindow.instance.getEngineComponent(InputGameEngineComponent);
     if (inputComponent === null) return;
+
     const mouse: Mouse | null = inputComponent.getDevice(Mouse);
     if (mouse) {
-      mouse.onAnyChange.removeObserver(() => this.onAnyChange);
+      mouse.onAnyChange.removeObserver(this.onAnyChangeObserver);
 
-      mouse.onLeftClickUp.removeObserver(() => this.onMouseLeftClickUp);
-      mouse.onLeftClickDown.removeObserver(() => this.onMouseLeftClickDown);
+      mouse.onLeftClickUp.removeObserver(this.onMouseLeftClickUpObserver);
+      mouse.onLeftClickDown.removeObserver(this.onMouseLeftClickDownObserver);
 
-      mouse.onRightClickUp.removeObserver(() => this.onMouseRightClickUp);
-      mouse.onRightClickDown.removeObserver(() => this.onMouseRightClickDown);
+      mouse.onRightClickUp.removeObserver(this.onMouseRightClickUpObserver);
+      mouse.onRightClickDown.removeObserver(this.onMouseRightClickDownObserver);
 
-      mouse.onMove.removeObserver(() => this.onMouseMove);
-      mouse.onScroll.removeObserver(() => this.onMouseScroll);
+      mouse.onMove.removeObserver(this.onMouseMoveObserver);
+      mouse.onScroll.removeObserver(this.onMouseScrollObserver);
     }
     const keyboard: Keyboard | null = inputComponent.getDevice(Keyboard);
     if (keyboard) {
-      keyboard.onAnyChange.removeObserver(() => this.onAnyChange);
-      keyboard.onKeyDown.removeObserver(() => this.onKeyboardKeyDown);
-      keyboard.onKeyUp.removeObserver(() => this.onKeyboardKeyUp);
+      keyboard.onAnyChange.removeObserver(this.onAnyChangeObserver);
+      keyboard.onKeyDown.removeObserver(this.onKeyboardKeyDownObserver);
+      keyboard.onKeyUp.removeObserver(this.onKeyboardKeyUpObserver);
     }
   }
 
@@ -63,7 +88,6 @@ export class DeviceInputBehavior extends InputBehavior {
    * @description Callbacks for the input events.
    * Override these methods to handle input events.
    */
-
   public onAnyChange(): void {}
   public onMouseLeftClickUp(): void {}
   public onMouseLeftClickDown(): void {}
@@ -71,6 +95,7 @@ export class DeviceInputBehavior extends InputBehavior {
   public onMouseRightClickDown(): void {}
   public onMouseMove(_data: MouseEvent): void {}
   public onMouseScroll(_data: number): void {}
+
   public onKeyboardKeyDown(_key: string): void {}
   public onKeyboardKeyUp(_key: string): void {}
 }
