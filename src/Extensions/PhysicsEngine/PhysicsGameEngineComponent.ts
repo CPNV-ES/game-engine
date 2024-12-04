fd64d0b897089ac6fb885822309c53b0817d7aa4
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-12-04 08:25:34 +0100

    feat: add a getAllColliders in the PhysicsGameEngineComponent

--- src/Extensions/PhysicsEngine/PhysicsGameEngineComponent.ts
+++ src/Extensions/PhysicsEngine/PhysicsGameEngineComponent.ts
@@ -1,5 +1,24 @@
-export class PhysicsGameEngineComponent {
+import { GameEngineComponent } from "../../Core/GameEngineComponent";
+import { Collider } from "./Collider";
+import { GameEngineWindow } from "../../Core/GameEngineWindow.ts";
+import { GameObject } from "../../Core/GameObject.ts";
+
+export class PhysicsGameEngineComponent extends GameEngineComponent {
+  rootObject: GameObject;
+  public onAttachedTo(_gameEngine: GameEngineWindow): void {
+    this.rootObject = GameEngineWindow.root;
+  }
+
+  private getAllColliders(): Collider[] {
+    return this.rootObject
+      .getAllChildren()
+      .reduce((colliders: Collider[], gameObject: GameObject) => {
+        return colliders.concat(gameObject.getBehaviors(Collider));
+      }, []);
+  }
+
   private tick(): void {
+    const allColliders = this.getAllColliders();
     //Check SAT collisions
   }
 }
