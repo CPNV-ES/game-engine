e68e09a823744e6fb74355a34d91ece14bf1aab9
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-12-04 16:57:01 +0100

    feat: add the sat collision check on the PhysicsGameEngineComponent ticks

--- src/Extensions/PhysicsEngine/PhysicsGameEngineComponent.ts
+++ src/Extensions/PhysicsEngine/PhysicsGameEngineComponent.ts
@@ -2,6 +2,8 @@ import { GameEngineComponent } from "../../Core/GameEngineComponent";
 import { Collider } from "./Collider";
 import { GameEngineWindow } from "../../Core/GameEngineWindow.ts";
 import { GameObject } from "../../Core/GameObject.ts";
+import { BoundingBoxCollider } from "./BoundingBoxCollider.ts";
+import { CollisionsHandler } from "./CollisionsHandler.ts";
 
 export class PhysicsGameEngineComponent extends GameEngineComponent {
   rootObject: GameObject;
@@ -13,15 +15,37 @@ export class PhysicsGameEngineComponent extends GameEngineComponent {
     this.tickInterval = setInterval(() => this.tick(), 100);
   }
 
-  private getAllColliders(): Collider[] {
+  private getAllBoundingBoxCollider(): Collider[] {
     return this.rootObject
       .getAllChildren()
       .reduce((colliders: Collider[], gameObject: GameObject) => {
-        return colliders.concat(gameObject.getBehaviors(Collider));
+        return colliders.concat(gameObject.getBehaviors(BoundingBoxCollider));
       }, []);
+  }
+
+  private getBoundingBoxColliderCollisions(
+    collider: BoundingBoxCollider,
+  ): Collider[] {
+    return this.getAllBoundingBoxCollider().reduce(
+      (collidingColliders: Collider[], otherCollider: BoundingBoxCollider) => {
+        if (
+          otherCollider !== collider &&
+          CollisionsHandler.isSATColliding(collider, otherCollider)
+        ) {
+          collidingColliders.push(otherCollider);
+        }
+        return collidingColliders;
+      },
+      [],
+    );
   }
 
   private tick(): void {
     //Check SAT collisions
+    this.getAllBoundingBoxCollider().forEach(
+      (collider: BoundingBoxCollider) => {
+        collider.collide(this.getBoundingBoxColliderCollisions(collider));
+      },
+    );
   }
 }
