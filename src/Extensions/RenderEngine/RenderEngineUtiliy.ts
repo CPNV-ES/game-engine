c04b0c6ab3484b82e3bbb3071b4f3fce07b0903b
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2024-12-04 11:07:24 +0100

    fix: model matrix was not modifying anything

--- src/Extensions/RenderEngine/RenderEngineUtiliy.ts
+++ src/Extensions/RenderEngine/RenderEngineUtiliy.ts
@@ -6,14 +6,14 @@ export class RenderEngineUtiliy {
    * Convert the Transform into a model matrix
    */
   public static toModelMatrix(transform: Transform): Float32Array {
-    const modelMatrix = mat4.identity();
-    mat4.translate(modelMatrix, modelMatrix, [
+    let modelMatrix = mat4.identity();
+    modelMatrix = mat4.translate(modelMatrix, [
       transform.position.x,
       transform.position.y,
       0,
     ]);
-    mat4.rotateZ(modelMatrix, transform.rotation);
-    mat4.scale(modelMatrix, modelMatrix, [
+    modelMatrix = mat4.rotateZ(modelMatrix, transform.rotation);
+    modelMatrix = mat4.scale(modelMatrix, [
       transform.scale.x,
       transform.scale.y,
       1,
