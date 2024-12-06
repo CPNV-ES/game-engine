dff8114bb299547a8b101974db676ec9bf8e1e0a
MinusW <pw07vck@eduvaud.ch>
2024-12-06 09:07:54 +0100

    test: test adding multiple devices and getting correct

--- test/Extensions/InputSystem/InputGameEngineComponent.test.ts
+++ test/Extensions/InputSystem/InputGameEngineComponent.test.ts
@@ -1,19 +1,40 @@
-import { describe, it, expect, beforeAll } from "vitest";
+import { describe, it, expect, beforeAll, beforeEach } from "vitest";
 import { InputGameEngineComponent } from "../../../src/Extensions/InputSystem/InputGameEngineComponent";
 import { Device } from "../../../src/Extensions/InputSystem/Device";
 import { MockDevice } from "./Mocks/MockDevice";
+import { Mouse } from "../../../src/Extensions/InputSystem/Mouse";
+import { Keyboard } from "../../../src/Extensions/InputSystem/Keyboard";
+import { InputUtility } from "./InputUtility";
 
 describe("InputGameEngineComponent", (): void => {
   let inputGameEngineComponent: InputGameEngineComponent;
   let mockDevice: Device;
 
-  beforeAll(() => {
+  beforeEach(() => {
     inputGameEngineComponent = new InputGameEngineComponent();
     mockDevice = new MockDevice();
+  });
+
+  beforeAll((): void => {
+    InputUtility.mockDocumentEventListeners();
   });
 
   it("should add a device and get a device", (): void => {
     inputGameEngineComponent.addDevice(mockDevice);
     expect(inputGameEngineComponent.getDevice(MockDevice)).toBe(mockDevice);
+  });
+
+  it("should add multiple devices and get correct device when calling all of them", (): void => {
+    // Given
+    const mouse: Device = new Mouse();
+    const keyboard: Device = new Keyboard();
+    inputGameEngineComponent.addDevice(mockDevice);
+    inputGameEngineComponent.addDevice(mouse);
+    inputGameEngineComponent.addDevice(keyboard);
+
+    // When and Then
+    expect(inputGameEngineComponent.getDevice(MockDevice)).toBe(mockDevice);
+    expect(inputGameEngineComponent.getDevice(Mouse)).toBe(mouse);
+    expect(inputGameEngineComponent.getDevice(Keyboard)).toBe(keyboard);
   });
 });
