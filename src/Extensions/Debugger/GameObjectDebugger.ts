a50fc122ebbf18c7f7b4c8717a98d44fa08d68cb
Philippe Moeckli <135646622+LeZderich@users.noreply.github.com>
2025-01-15 14:29:30 +0100

    feat: implement a debugger using the lil-gui to display the attributes of game objects

---
+++ src/Extensions/Debugger/GameObjectDebugger.ts
@@ -0,0 +1,127 @@
+import { Behavior } from "../../Core/Behavior";
+import { GameEngineWindow } from "../../Core/GameEngineWindow";
+import { GameObject } from "../../Core/GameObject";
+import GUI from "lil-gui";
+
+/**
+ * Class responsible for debugging game objects using a GUI.
+ * @param {HTMLElement} HtmlElement - The root HTML element to contain the GUI.
+ *
+ * @example
+ * ```typescript
+ * const debugContainer = document.getElementById('debug-container');
+ * const gameObjectDebugger = new GameObjectDebugger(debugContainer);
+ *
+ * gameObjectDebugger.render();
+ * gameObjectDebugger.title("Game Object Debugger");
+ * ```
+ */
+export class GameObjectDebugger {
+  private readonly _debugContainer: HTMLElement;
+  private _debugGUI: GUI;
+
+  constructor(HtmlElement: HTMLElement) {
+    this._debugContainer = HtmlElement;
+    this._debugGUI = new GUI({
+      container: this._debugContainer,
+    });
+  }
+
+  /**
+   * Sets the title of the GUI.
+   * @param {string} title - The title to set.
+   */
+  public title(title: string): void {
+    this._debugGUI.title(title);
+  }
+
+  /**
+   * Renders the GUI for the game objects.
+   * The root game object from the GameEngineWindow is rendered with all its children and their behaviors.
+   */
+  public render(): void {
+    this._debugGUI.destroy();
+    this._debugGUI = new GUI({
+      container: this._debugContainer,
+    });
+    this.renderGameObjects(GameEngineWindow.instance.root, this._debugGUI);
+  }
+
+  private renderGameObjects(gameObject: GameObject, gui: GUI): void {
+    const gameObjectFolder: GUI = gui.addFolder(gameObject.name);
+
+    gameObjectFolder
+      .add(gameObject, "name")
+      .name("Name")
+      .onFinishChange((value: string): void => {
+        gameObject.name = value;
+        gameObjectFolder.title(gameObject.name);
+      })
+      .updateDisplay();
+
+    gameObjectFolder
+      .add(gameObject.transform.position, "x")
+      .name("Position X")
+      .onChange((value: number): void => {
+        gameObject.transform.position.x = value;
+      });
+
+    gameObjectFolder
+      .add(gameObject.transform.position, "y")
+      .name("Position Y")
+      .onChange((value: number): void => {
+        gameObject.transform.position.y = value;
+      });
+
+    gameObjectFolder
+      .add(gameObject.transform.scale, "x")
+      .name("Scale X")
+      .onChange((value: number): void => {
+        gameObject.transform.scale.x = value;
+      });
+
+    gameObjectFolder
+      .add(gameObject.transform.scale, "y")
+      .name("Scale Y")
+      .onChange((value: number): void => {
+        gameObject.transform.scale.y = value;
+      });
+
+    gameObjectFolder
+      .add(gameObject.transform, "rotation")
+      .name("Rotation")
+      .onChange((value: number): void => {
+        gameObject.transform.rotation = value;
+      });
+
+    gameObjectFolder
+      .add(
+        {
+          addChild: (): void => {
+            const newChild: GameObject = new GameObject();
+            gameObject.addChild(newChild);
+            this.renderGameObjects(newChild, gameObjectFolder);
+          },
+        },
+        "addChild",
+      )
+      .name("Add Child");
+    if (gameObject.parent) {
+      gameObjectFolder
+        .add(
+          {
+            removeChild: (): void => {
+              gameObject.parent?.removeChild(gameObject);
+              gameObjectFolder.destroy();
+            },
+          },
+          "removeChild",
+        )
+        .name("Remove");
+    }
+
+    gameObject.children.forEach((child: GameObject): void => {
+      this.renderGameObjects(child, gameObjectFolder);
+    });
+  }
+}
