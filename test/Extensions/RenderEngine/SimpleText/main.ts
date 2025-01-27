183dcfa3e96e30f127b5df48a526fd092a67539f
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2025-01-27 15:00:25 +0100

    test: add simple text as integration test

---
+++ test/Extensions/RenderEngine/SimpleText/main.ts
@@ -0,0 +1,28 @@
+import { GameEngineWindow } from "../../../../src/Core/GameEngineWindow.ts";
+import { RenderGameEngineComponent } from "../../../../src/Extensions/RenderEngine/RenderGameEngineComponent.ts";
+import { GameObject } from "../../../../src/Core/GameObject.ts";
+import { Camera } from "../../../../src/Extensions/RenderEngine/Camera.ts";
+import { TextRenderBehavior } from "../../../../src/Extensions/RenderEngine/Text/TextRenderBehavior";
+
+const canvas: HTMLCanvasElement =
+  document.querySelector<HTMLCanvasElement>("#app")!;
+
+const gameEngineWindow: GameEngineWindow = GameEngineWindow.instance;
+const renderComponent: RenderGameEngineComponent =
+  new RenderGameEngineComponent(canvas, navigator.gpu);
+
+gameEngineWindow.addGameComponent(renderComponent);
+
+const go = new GameObject();
+gameEngineWindow.root.addChild(go);
+
+go.addBehavior(
+  new TextRenderBehavior(
+    renderComponent,
+    "/test/Extensions/RenderEngine/SimpleText/Sprunthrax/Sprunthrax-SemiBold-msdf.json",
+  ),
+);
+
+const cameraGo = new GameObject();
+gameEngineWindow.root.addChild(cameraGo);
+cameraGo.addBehavior(new Camera());
