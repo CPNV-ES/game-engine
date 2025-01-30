6a574f2997d41764c631e73ac78ea9b12e12cae8
Philippe Moeckli <135646622+LeZderich@users.noreply.github.com>
2025-01-30 19:22:09 +0100

    refactor: separate debugger logic into it's own module

--- test/Extensions/Debugger/main.ts
+++ test/Extensions/Debugger/main.ts
@@ -1,6 +1,5 @@
 import { GameEngineWindow } from "../../../src/Core/GameEngineWindow";
 import { GameObject } from "../../../src/Core/GameObject";
-import { GameObjectDebugger } from "../../../src/Extensions/Debugger/GameObjectDebugger";
 import { TestBehavior } from "../../Core/Mocks/TestBehavior";
 import { TestInputBehavior } from "../../Core/Mocks/TestInputBehavior";
 import { TestLogicBehavior } from "../../Core/Mocks/TestLogicBehavior";
@@ -23,37 +22,3 @@ childGameObject1.addChild(grandChildGameObject);
 childGameObject1.addBehavior(new TestBehavior());
 childGameObject1.addBehavior(new TestInputBehavior());
 childGameObject1.addBehavior(new TestLogicBehavior());
-
-const debuggerContainer: HTMLElement = document.getElementById("debug");
-let debuggerInstance: GameObjectDebugger;
-
-if (debuggerContainer) {
-  debuggerInstance = new GameObjectDebugger(debuggerContainer);
-  debuggerInstance.render();
-  debuggerInstance.title("Game Object Debugger");
-}
-
-const debugElement: HTMLElement = document.getElementById("container");
-let isDragging: boolean = false;
-let offsetX: number, offsetY: number;
-
-debugElement?.addEventListener("mousedown", (event: MouseEvent): void => {
-  if (event.target === debugElement) {
-    isDragging = true;
-    offsetX = event.clientX - debugElement.offsetLeft;
-    offsetY = event.clientY - debugElement.offsetTop;
-    debugElement.style.cursor = "grabbing";
-  }
-});
-
-document.addEventListener("mousemove", (event: MouseEvent): void => {
-  if (isDragging && debugElement) {
-    debugElement.style.left = `${event.clientX - offsetX}px`;
-    debugElement.style.top = `${event.clientY - offsetY}px`;
-  }
-});
-
-document.addEventListener("mouseup", (): void => {
-  isDragging = false;
-  debugElement?.style.removeProperty("cursor");
-});
