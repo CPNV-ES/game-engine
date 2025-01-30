a8750a28240c3bae81d28385b39a12f8feafe82b
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2025-01-30 11:53:55 +0100

    feat: add Rigidbody cass in the physics engine

---
+++ src/Extensions/PhysicsEngine/Rigidbody.ts
@@ -0,0 +1,19 @@
+import { Vector2 } from "../../Core/MathStructures/Vector2.ts";
+import { Collider } from "./Collider.ts";
+
+export class Rigidbody {
+  public position: Vector2;
+  public velocity: Vector2 = new Vector2(0, 0);
+  public mass: number = 1;
+  public restitution: number = 1;
+  private _collider: Collider;
+
+  public get collider(): Collider {
+    return this._collider;
+  }
+
+  constructor(collider: Collider, position: Vector2) {
+    this._collider = collider;
+    this.position = position;
+  }
+}
