364be9707713207d3118cd6dc12e331ecfdc9bfb
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2025-02-13 11:29:58 +0100

    feat: add a method to get the opposite of a collision (in order to resolve it both ways)

--- src/Extensions/PhysicsEngine/Colliders/Collision.ts
+++ src/Extensions/PhysicsEngine/Colliders/Collision.ts
@@ -1,8 +1,10 @@
 import { Vector2 } from "../../../Core/MathStructures/Vector2.ts";
+import { Collider } from "./Collider.ts";
 
 export class Collision {
   private _depth: number;
   private _normal: Vector2;
+  private _collidingWith: Collider;
 
   public get depth(): number {
     return this._depth;
@@ -12,8 +14,13 @@ export class Collision {
     return this._normal;
   }
 
-  constructor(depth: number, normal: Vector2) {
+  constructor(depth: number, normal: Vector2, collidingWith: Collider) {
     this._depth = depth;
     this._normal = normal;
+    this._collidingWith = collidingWith;
+  }
+
+  public getOpposite() {
+    return new Collision(-this._depth, this._normal, this._collidingWith);
   }
 }
