1bbf5edee896e7838a0571f39069797d8287d14e
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2025-02-13 10:24:10 +0100

    test: test mesh rendering with bust

---
+++ test/Extensions/RenderEngine/MeshDrawing/main.ts
@@ -0,0 +1,49 @@
+import { GameEngineWindow } from "../../../../src/Core/GameEngineWindow.ts";
+import { RenderGameEngineComponent } from "../../../../src/Extensions/RenderEngine/RenderGameEngineComponent.ts";
+import { MeshRenderBehavior } from "../../../../src/Extensions/RenderEngine/MeshBased/MeshRenderBehavior.ts";
+import { GameObject } from "../../../../src/Core/GameObject.ts";
+import { Camera } from "../../../../src/Extensions/RenderEngine/Camera.ts";
+import { Sprunk } from "../../../../src/Core/Initialisation/Sprunk";
+import { ObjLoader } from "../../../../src/Extensions/RenderEngine/MeshBased/ObjLoader";
+
+import BasicVertexMVPWithUV from "../../../../src/Extensions/RenderEngine/BasicShaders/BasicVertexMVPWithUV.vert.wgsl?raw";
+import BasicTextureSample from "../../../../src/Extensions/RenderEngine/BasicShaders/BasicTextureSample.frag.wgsl?raw";
+import { FreeLookCameraController } from "../../../ExampleBehaviors/FreeLookCameraController";
+import { FreeLookCameraKeyboardMouseInput } from "../../../ExampleBehaviors/FreeLookCameraKeyboardMouseInput";
+import { Camera3D } from "../../../ExampleBehaviors/Camera3D";
+import { InputGameEngineComponent } from "../../../../src/Extensions/InputSystem/InputGameEngineComponent";
+
+const canvas: HTMLCanvasElement =
+  document.querySelector<HTMLCanvasElement>("#app")!;
+
+const gameEngineWindow: GameEngineWindow = Sprunk.newGame(canvas, false, [
+  "RenderGameEngineComponent",
+  "InputGameEngineComponent",
+]);
+const renderComponent: RenderGameEngineComponent =
+  gameEngineWindow.getEngineComponent(RenderGameEngineComponent)!;
+const inputComponent: InputGameEngineComponent =
+  gameEngineWindow.getEngineComponent(InputGameEngineComponent)!;
+
+const go = new GameObject();
+gameEngineWindow.root.addChild(go);
+
+ObjLoader.load("/test/CommonResources/bust.obj").then((obj) => {
+  go.addBehavior(
+    new MeshRenderBehavior(
+      renderComponent,
+      obj,
+      "/test/CommonResources/sprunk.png",
+      BasicVertexMVPWithUV,
+      BasicTextureSample,
+    ),
+  );
+});
+
+go.transform.position.x = 2;
+
+const cameraGo = new GameObject("Camera");
+cameraGo.addBehavior(new FreeLookCameraController());
+cameraGo.addBehavior(new FreeLookCameraKeyboardMouseInput(inputComponent));
+cameraGo.addBehavior(new Camera3D(renderComponent));
+gameEngineWindow.root.addChild(cameraGo);
