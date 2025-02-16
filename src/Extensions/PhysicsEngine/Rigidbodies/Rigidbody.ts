ce601fc05d861c82fdfcbcaa5fbde1be3d095b1d
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2025-02-16 17:38:33 +0100

    refactor: add LogicBehaviour as parent of Rigidbody

--- src/Extensions/PhysicsEngine/Rigidbodies/Rigidbody.ts
+++ src/Extensions/PhysicsEngine/Rigidbodies/Rigidbody.ts
@@ -1,9 +1,9 @@
 import { Vector2 } from "../../../Core/MathStructures/Vector2.ts";
 import { Collider } from "../Colliders/Collider.ts";
 import { Collision } from "../Colliders/Collision.ts";
+import { LogicBehavior } from "../../../Core/LogicBehavior.ts";
 
-export class Rigidbody {
-  public position: Vector2;
+export class Rigidbody extends LogicBehavior<void> {
   public velocity: Vector2 = new Vector2(0, 0);
   public mass: number = 1;
   public restitution: number = 1;
@@ -13,9 +13,11 @@ export class Rigidbody {
     return this._collider;
   }
 
-  constructor(collider: Collider, position: Vector2) {
+  constructor(collider: Collider) {
+    super();
     this._collider = collider;
-    this.position = position;
+    // TODO: remove this hack by fixing stuff in the inheritance chain
+    this.data = true;
 
     this._collider.onDataChanged.addObserver((data: Collision[]) =>
       this.resolveCollisions(data),
@@ -23,15 +25,16 @@ export class Rigidbody {
   }
 
   public resolveCollisions(collisions: Collision[]) {
+    if (this.gameObject === undefined) return;
+
     collisions.forEach((collision: Collision) => {
       this.resolveCollision(collision);
     });
   }
 
   public resolveCollision(collision: Collision) {
-    this._collider.gameObject.transform.position =
-      this._collider.gameObject.transform.position.sub(
-        collision.normal.scale(collision.depth / 2),
-      );
+    this.gameObject.transform.position = this.gameObject.transform.position.sub(
+      collision.normal.scale(collision.depth / 2),
+    );
   }
 }
