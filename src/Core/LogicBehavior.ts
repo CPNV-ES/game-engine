f5a78acb9a8896655a72c64715f84e02a8a021bd
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2025-02-27 14:11:52 +0100

    feat: add data initialization check in LogicBehavior and implement isVoid method

--- src/Core/LogicBehavior.ts
+++ src/Core/LogicBehavior.ts
@@ -14,12 +14,16 @@ export class LogicBehavior<T> extends Behavior {
   override setup(attachedOn: GameObject): void {
     this.gameObject = attachedOn;
     super.setup(attachedOn);
-    if (!this.data) {
+    if (this.data === undefined && !this.isVoid<T>()) {
       throw new Error("Data must be initialized in the constructor.");
     }
   }
 
   protected notifyDataChanged(): void {
     this.onDataChanged.emit(this.data);
+  }
+
+  private isVoid<T>(): boolean {
+    return (undefined as unknown as T) === undefined;
   }
 }
