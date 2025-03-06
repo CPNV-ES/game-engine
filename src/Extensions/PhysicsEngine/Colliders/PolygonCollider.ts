74e3627c1d03f9d11e29b311c3a7654d4f584c20
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2025-03-06 11:45:54 +0100

    fix: replace the getGravitationCenter method with a correct mathematical approach

--- src/Extensions/PhysicsEngine/Colliders/PolygonCollider.ts
+++ src/Extensions/PhysicsEngine/Colliders/PolygonCollider.ts
@@ -14,14 +14,48 @@ export class PolygonCollider extends Collider {
 
   /**
    * Get the gravitation center of the polygon
+   *
+   * @description
+   * ### The centroid \( (C_x, C_y) \) of a polygon is given by:
+   * - $C_x = \frac{1}{6A} \sum_{i=1}^{n} (x_i + x_{i+1})(x_i y_{i+1} - x_{i+1} y_i)$
+   * - $C_y = \frac{1}{6A} \sum_{i=1}^{n} (y_i + y_{i+1})(x_i y_{i+1} - x_{i+1} y_i)$
+   * ### Where:
+   * - $A$ is the area of the polygon.
+   * - $(x_i, y_i)$ are the coordinates of the vertices.
+   * - $n$ is the number of vertices.
    */
   public getGravitationCenter(): Vector2 {
-    return this.vertices
-      .reduce(
-        (sum: Vector2, vertex: Vector2) => sum.add(vertex),
-        new Vector2(0, 0),
-      )
-      .scale(1 / this.vertices.length);
+    let area = 0;
+    let centroidX = 0;
+    let centroidY = 0;
+
+    for (let i = 0; i < this.vertices.length; i++) {
+      // Get the current vertex
+      const x1 = this.vertices[i].x;
+      const y1 = this.vertices[i].y;
+
+      // Get the next vertex
+      const x2 = this.vertices[(i + 1) % this.vertices.length].x;
+      const y2 = this.vertices[(i + 1) % this.vertices.length].y;
+
+      const crossProduct = x1 * y2 - x2 * y1;
+
+      // Add the cross product to the total area
+      area += crossProduct;
+
+      // Calculate the contribution of the current edge to the centroid's x and y coordinates
+      // This is based on the weighted average of the vertices
+      centroidX += (x1 + x2) * crossProduct;
+      centroidY += (y1 + y2) * crossProduct;
+    }
+
+    area /= 2;
+
+    // Normalize the centroid coordinates by dividing by 6 times the area (part of mathematical formula)
+    centroidX /= 6 * area;
+    centroidY /= 6 * area;
+
+    return new Vector2(centroidX, centroidY);
   }
 
   /**
