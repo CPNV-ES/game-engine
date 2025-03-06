d0fdfbb52d27799af9498e59d584a52ea882184f
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2025-03-06 12:32:18 +0100

    refactor: remove unnecessary error throwing and simplify data initialization in LogicBehavior

--- src/Core/LogicBehavior.ts
+++ src/Core/LogicBehavior.ts
@@ -11,19 +11,18 @@ export class LogicBehavior<T> extends Behavior {
   protected gameObject!: GameObject;
   protected data!: T;
 
+  /**
+   * Like onEnable, but should only be for internal use, so some non-abstact direct behaviors could expose attachedOn to the user.
+   * AT THE END OF THIS FUNCTION, WE EXPECT this.data to be filled (and not undefined). This isn't an assertion becaus we support T as void.
+   * @param attachedOn The GameObject this behavior is attached to.
+   * @throws Error if the behavior is already attached to a GameObject.
+   */
   override setup(attachedOn: GameObject): void {
     this.gameObject = attachedOn;
     super.setup(attachedOn);
-    if (this.data === undefined && !this.isVoid<T>()) {
-      throw new Error("Data must be initialized in the constructor.");
-    }
   }
 
   protected notifyDataChanged(): void {
     this.onDataChanged.emit(this.data);
-  }
-
-  private isVoid<T>(): boolean {
-    return (undefined as unknown as T) === undefined;
   }
 }
