ada06767c043251914f753b8858e17b0a3280494
Philippe Moeckli <135646622+LeZderich@users.noreply.github.com>
2025-03-08 13:02:34 +0100

    feat: handle the behavior a gamepad in the input component

--- src/Extensions/InputSystem/DeviceInputBehavior.ts
+++ src/Extensions/InputSystem/DeviceInputBehavior.ts
@@ -3,6 +3,8 @@ import { InputGameEngineComponent } from "@extensions/InputSystem/InputGameEngin
 import { Mouse } from "@extensions/InputSystem/Mouse.ts";
 import { Keyboard } from "@extensions/InputSystem/Keyboard.ts";
 import { Vector2 } from "@core/MathStructures/Vector2.ts";
+import { GamepadDevice } from "@extensions/InputSystem/GamepadDevice.ts";
+import { XboxGamepad } from "@extensions/InputSystem/Gamepads/XboxGamepad.ts";
 
 /**
  * @class DeviceInputBehavior
@@ -24,6 +26,20 @@ export class DeviceInputBehavior extends InputBehavior {
     this.onKeyboardKeyDown(data);
   private onKeyboardKeyUpObserver = (data: string) =>
     this.onKeyboardKeyUp(data);
+
+  private onGamepadButtonDownObserver = (data: number) =>
+    this.onGamepadButtonDown(data);
+  private onGamepadButtonUpObserver = (data: number) =>
+    this.onGamepadButtonUp(data);
+  private onGamepadAxisChangeObserver = (data: {
+    axis: number;
+    value: number;
+  }) => this.onGamepadAxisChange(data);
+  private onGamepadConnectedObserver = (gamepad: GamepadDevice | XboxGamepad) =>
+    this.onGamepadConnected(gamepad);
+  private onGamepadDisconnectedObserver = (
+    gamepad: GamepadDevice | XboxGamepad,
+  ) => this.onGamepadDisconnected(gamepad);
 
   /**
    * Creates an instance of DeviceInputBehavior.
@@ -59,6 +75,22 @@ export class DeviceInputBehavior extends InputBehavior {
       keyboard.onKeyDown.addObserver(this.onKeyboardKeyDownObserver);
       keyboard.onKeyUp.addObserver(this.onKeyboardKeyUpObserver);
     }
+
+    const gamepadManager = this.inputEngineComponent.getGamepadManager();
+    if (gamepadManager) {
+      gamepadManager.onGamepadConnected.addObserver(
+        this.onGamepadConnectedObserver,
+      );
+      gamepadManager.onGamepadDisconnected.addObserver(
+        this.onGamepadDisconnectedObserver,
+      );
+
+      // Add observers to existing gamepads
+      const gamepads = gamepadManager.getAllGamepads();
+      gamepads.forEach((gamepad: GamepadDevice | XboxGamepad) => {
+        this.setupGamepadObservers(gamepad);
+      });
+    }
   }
 
   /**
@@ -87,6 +119,35 @@ export class DeviceInputBehavior extends InputBehavior {
       keyboard.onKeyDown.removeObserver(this.onKeyboardKeyDownObserver);
       keyboard.onKeyUp.removeObserver(this.onKeyboardKeyUpObserver);
     }
+
+    const gamepadManager = this.inputEngineComponent.getGamepadManager();
+    if (gamepadManager) {
+      gamepadManager.onGamepadConnected.removeObserver(
+        this.onGamepadConnectedObserver,
+      );
+      gamepadManager.onGamepadDisconnected.removeObserver(
+        this.onGamepadDisconnectedObserver,
+      );
+
+      const gamepads = gamepadManager.getAllGamepads();
+      gamepads.forEach((gamepad: GamepadDevice | XboxGamepad) => {
+        this.removeGamepadObservers(gamepad);
+      });
+    }
+  }
+
+  private setupGamepadObservers(gamepad: GamepadDevice | XboxGamepad): void {
+    gamepad.onAnyChange.addObserver(this.onAnyChangeObserver);
+    gamepad.onButtonDown.addObserver(this.onGamepadButtonDownObserver);
+    gamepad.onButtonUp.addObserver(this.onGamepadButtonUpObserver);
+    gamepad.onAxisChange.addObserver(this.onGamepadAxisChangeObserver);
+  }
+
+  private removeGamepadObservers(gamepad: GamepadDevice | XboxGamepad): void {
+    gamepad.onAnyChange.removeObserver(this.onAnyChangeObserver);
+    gamepad.onButtonDown.removeObserver(this.onGamepadButtonDownObserver);
+    gamepad.onButtonUp.removeObserver(this.onGamepadButtonUpObserver);
+    gamepad.onAxisChange.removeObserver(this.onGamepadAxisChangeObserver);
   }
 
   /**
@@ -103,4 +164,10 @@ export class DeviceInputBehavior extends InputBehavior {
 
   public onKeyboardKeyDown(_key: string): void {}
   public onKeyboardKeyUp(_key: string): void {}
+
+  public onGamepadButtonDown(_buttonIndex: number): void {}
+  public onGamepadButtonUp(_buttonIndex: number): void {}
+  public onGamepadAxisChange(_data: { axis: number; value: number }): void {}
+  public onGamepadConnected(_gamepad: GamepadDevice | XboxGamepad): void {}
+  public onGamepadDisconnected(_gamepad: GamepadDevice | XboxGamepad): void {}
 }
--- src/Extensions/InputSystem/InputGameEngineComponent.ts
+++ src/Extensions/InputSystem/InputGameEngineComponent.ts
@@ -1,5 +1,7 @@
 import { GameEngineComponent } from "@core/GameEngineComponent.ts";
 import { Device } from "@extensions/InputSystem/Device.ts";
+import { GamepadManager } from "@extensions/InputSystem/GamepadManager.ts";
+import { GamepadDevice } from "@extensions/InputSystem/Gamepad.ts";
 
 /**
  * A game engine component that manages input devices.
@@ -8,6 +10,16 @@ import { Device } from "@extensions/InputSystem/Device.ts";
  */
 export class InputGameEngineComponent extends GameEngineComponent {
   private _devices: Device[] = [];
+  private _gamepadManager: GamepadManager;
+
+  constructor() {
+    super();
+    this._gamepadManager = new GamepadManager();
+    const gamepads: GamepadDevice[] = this._gamepadManager.getAllGamepads();
+    gamepads.forEach((gamepad) => {
+      this.addDevice(gamepad);
+    });
+  }
 
   /**
    * Gets a device of a specified class inheriting Device.
@@ -17,6 +29,13 @@ export class InputGameEngineComponent extends GameEngineComponent {
   ): T | null {
     return (this._devices.find((device) => device instanceof deviceClass) ||
       null) as T;
+  }
+
+  /**
+   * Gets the gamepad manager instance.
+   */
+  public getGamepadManager(): GamepadManager {
+    return this._gamepadManager;
   }
 
   /**
