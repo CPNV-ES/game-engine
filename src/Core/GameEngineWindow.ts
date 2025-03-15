32c8778e38007fd265edaac848bf7bd042c74c8b
eliott <eliott.jaquier@gmail.com>
2025-03-15 20:05:14 +0100

    feat: integrate DependencyContainer into GameEngineWindow for dependency management

--- src/Core/GameEngineWindow.ts
+++ src/Core/GameEngineWindow.ts
@@ -2,16 +2,17 @@ import { GameEngineComponent } from "@core/GameEngineComponent.ts";
 import { Ticker } from "@core/Tickers/Ticker.ts";
 import { Behavior } from "@core/Behavior.ts";
 import { RootGameObject } from "@core/RootGameObject.ts";
+import { DependencyContainer } from "@core/DependencyInjection/DependencyContainer.ts";
 
 /**
  * @class GameEngineWindow
  * @description Singleton class that represents the game engine window.
- * @property {GameEngineComponent[]} _engineComponents - Array of GameEngineComponents that are attached to the game engine window.
  */
 export class GameEngineWindow {
   private _engineComponents: GameEngineComponent[] = [];
   private _root: RootGameObject = new RootGameObject(this);
   private _logicTicker: Ticker;
+  private _injectionContainer: DependencyContainer = new DependencyContainer();
 
   /**
    * Creates an instance of GameEngineWindow.
@@ -33,6 +34,14 @@ export class GameEngineWindow {
   }
 
   /**
+   * @description Dependency injection container for the game engine window.
+   * Can be used to store engine-wide dependencies and global logic components.
+   */
+  public get injectionContainer(): DependencyContainer {
+    return this._injectionContainer;
+  }
+
+  /**
    * @param component
    * @description Adds a GameEngineComponent to the game engine window unless it is already attached.
    */
@@ -40,6 +49,10 @@ export class GameEngineWindow {
     if (this._engineComponents.includes(component)) return;
     this._engineComponents.push(component);
     component.onAttachedTo(this);
+    this._injectionContainer.registerWithClassName(
+      component.constructor.name,
+      component,
+    );
   }
 
   /**
@@ -60,7 +73,12 @@ export class GameEngineWindow {
    */
   public dispose(): void {
     this._logicTicker.onTick.removeObservers();
-    this._engineComponents.forEach((component) => component.onDetached());
+    this._engineComponents.forEach((component) => {
+      component.onDetached();
+      this._injectionContainer.unregisterWithClassName(
+        component.constructor.name,
+      );
+    });
     this._engineComponents = [];
     this._root.destroy();
   }
