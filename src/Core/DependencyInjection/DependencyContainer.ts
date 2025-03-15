22be1981ae9bb3a1c0791af5c645c43c751db87d
eliott <eliott.jaquier@gmail.com>
2025-03-15 19:56:26 +0100

    feat: improve DependencyContainer to use class names for registration and resolution + support for unregister

--- src/Core/DependencyInjection/DependencyContainer.ts
+++ src/Core/DependencyInjection/DependencyContainer.ts
@@ -1,42 +1,52 @@
-import { INJECT_METADATA_KEY } from "@core/DependencyInjection/Inject.ts";
-
 /**
  * A simple dependency container that allows for registering and resolving dependencies.
  */
 export class DependencyContainer {
-  private dependencies = new Map<Function, any>();
+  private dependencies = new Map<string, any>();
 
   /**
    * Register an instance of a class to be resolved when requested.
    * @param token - The token of the class to register (in form of a constructor).
    * @param instance - The instance to register.
    */
-  register<T>(token: abstract new (...args: any[]) => T, instance: T): void {
-    this.dependencies.set(token, instance);
+  register<T>(token: new (...args: any[]) => T, instance: T): void {
+    this.dependencies.set(token.name, instance);
+  }
+
+  /**
+   * Register an instance of a class to be resolved when requested.
+   * @param className - The constructor name of the class to register.
+   * @param instance - The instance to register.
+   */
+  registerWithClassName<T>(className: string, instance: T): void {
+    this.dependencies.set(className, instance);
+  }
+
+  /**
+   * Unregister a class.
+   * @param token
+   */
+  unregister<T>(token: new (...args: any[]) => T): void {
+    this.dependencies.delete(token.name);
+  }
+
+  /**
+   * Unregister an instance of a class.
+   * @param className - The constructor name of the class to unregister.
+   */
+  unregisterWithClassName(className: string): void {
+    this.dependencies.delete(className);
   }
 
   /**
    * Resolve an instance of a class.
    * @param token - The token of the class to resolve (in form of a constructor).
    */
-  resolve<T>(token: abstract new (...args: any[]) => T): T {
-    const instance = this.dependencies.get(token);
+  resolve<T>(token: new (...args: any[]) => T): T {
+    const instance = this.dependencies.get(token.name);
     if (!instance) {
       throw new Error(`No instance registered for ${token.name}`);
     }
     return instance;
-  }
-
-  /**
-   * Fill all dependencies with a target object.
-   * @param target - The target object to fill dependencies for.
-   */
-  fillDependencies(target: any): void {
-    for (const key of Object.keys(target)) {
-      const token = Reflect.getMetadata(INJECT_METADATA_KEY, target, key);
-      if (token) {
-        target[key] = this.resolve(token);
-      }
-    }
   }
 }
--- test/Core/DependencyInjection/DependencyContainer.test.ts
+++ test/Core/DependencyInjection/DependencyContainer.test.ts
@@ -1,15 +1,9 @@
 import { describe, it, expect } from "vitest";
 import { DependencyContainer } from "@core/DependencyInjection/DependencyContainer.ts";
-import { INJECT_METADATA_KEY } from "@core/DependencyInjection/Inject.ts";
 
 // Define a test class with dependencies
 class TestDependency {
   public value: string = "Hello, World!";
-}
-
-class TestClass {
-  @Reflect.metadata(INJECT_METADATA_KEY, TestDependency)
-  public dependency!: TestDependency;
 }
 
 describe("DependencyContainer", () => {
@@ -35,44 +29,5 @@ describe("DependencyContainer", () => {
     expect(() => container.resolve(TestDependency)).toThrowError(
       `No instance registered for TestDependency`,
     );
-  });
-
-  it("should fill dependencies in a target object", () => {
-    const container = new DependencyContainer();
-    const dependency = new TestDependency();
-
-    // Register the dependency
-    container.register(TestDependency, dependency);
-
-    // Create a target object with a dependency
-    const target = new TestClass();
-
-    // Fill dependencies in the target object
-    container.fillDependencies(target);
-
-    // Verify the dependency was injected correctly
-    expect(target.dependency).toBe(dependency);
-    expect(target.dependency.value).toBe("Hello, World!");
-  });
-
-  it("should not fill dependencies for properties without metadata", () => {
-    const container = new DependencyContainer();
-    const dependency = new TestDependency();
-
-    // Register the dependency
-    container.register(TestDependency, dependency);
-
-    // Create a target object with a non-injected property
-    class TestClassWithoutMetadata {
-      public nonInjectedProperty: string = "No dependency here!";
-    }
-
-    const target = new TestClassWithoutMetadata();
-
-    // Fill dependencies in the target object
-    container.fillDependencies(target);
-
-    // Verify the non-injected property remains unchanged
-    expect(target.nonInjectedProperty).toBe("No dependency here!");
   });
 });
