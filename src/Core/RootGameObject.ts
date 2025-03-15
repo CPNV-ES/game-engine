a5067b2a18377343554c9d81a8e64a945777b72f
eliott <eliott.jaquier@gmail.com>
2025-03-15 21:30:58 +0100

    feat: initialize _root property in RootGameObject constructor

--- src/Core/RootGameObject.ts
+++ src/Core/RootGameObject.ts
@@ -14,5 +14,6 @@ export class RootGameObject extends GameObject {
   constructor(gameEngineWindow: GameEngineWindow) {
     super("Root");
     this.gameWindowInstance = gameEngineWindow;
+    this._root = this;
   }
 }
