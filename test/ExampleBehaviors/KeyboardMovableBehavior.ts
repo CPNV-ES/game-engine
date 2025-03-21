3bcaf20525be2a37d1e4ed77500b16d492ea6bea
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2025-03-21 22:09:33 +0100

    test: refactor the movable logic from the physics sandbox in order to use forces only

--- test/ExampleBehaviors/KeyboardMovableBehavior.ts
+++ test/ExampleBehaviors/KeyboardMovableBehavior.ts
@@ -15,16 +15,16 @@ export class KeyboardMovableBehavior extends DeviceInputBehavior {
     console.log("Key down: " + _key);
     switch (_key) {
       case "ArrowUp":
-        this._movableLogicBehavior.translationSpeed.add(new Vector3(0, -10, 0));
+        this._movableLogicBehavior.translationForce.add(new Vector3(0, -10, 0));
         break;
       case "ArrowDown":
-        this._movableLogicBehavior.translationSpeed.add(new Vector3(0, 10, 0));
+        this._movableLogicBehavior.translationForce.add(new Vector3(0, 10, 0));
         break;
       case "ArrowLeft":
-        this._movableLogicBehavior.translationSpeed.add(new Vector3(-10, 0, 0));
+        this._movableLogicBehavior.translationForce.add(new Vector3(-10, 0, 0));
         break;
       case "ArrowRight":
-        this._movableLogicBehavior.translationSpeed.add(new Vector3(10, 0, 0));
+        this._movableLogicBehavior.translationForce.add(new Vector3(10, 0, 0));
         break;
     }
   }
@@ -33,16 +33,16 @@ export class KeyboardMovableBehavior extends DeviceInputBehavior {
     if (!this._movableLogicBehavior) return;
     switch (_key) {
       case "ArrowUp":
-        this._movableLogicBehavior.translationSpeed.add(new Vector3(0, 10, 0));
+        this._movableLogicBehavior.translationForce.add(new Vector3(0, 10, 0));
         break;
       case "ArrowDown":
-        this._movableLogicBehavior.translationSpeed.add(new Vector3(0, -10, 0));
+        this._movableLogicBehavior.translationForce.add(new Vector3(0, -10, 0));
         break;
       case "ArrowLeft":
-        this._movableLogicBehavior.translationSpeed.add(new Vector3(10, 0, 0));
+        this._movableLogicBehavior.translationForce.add(new Vector3(10, 0, 0));
         break;
       case "ArrowRight":
-        this._movableLogicBehavior.translationSpeed.add(new Vector3(-10, 0, 0));
+        this._movableLogicBehavior.translationForce.add(new Vector3(-10, 0, 0));
         break;
     }
   }
--- test/ExampleBehaviors/MovableLogicBehavior.ts
+++ test/ExampleBehaviors/MovableLogicBehavior.ts
@@ -1,5 +1,6 @@
 import { LogicBehavior } from "@core/LogicBehavior.ts";
 import { Vector3 } from "@core/MathStructures/Vector3.ts";
+import { Rigidbody } from "@extensions/PhysicsEngine/Rigidbodies/Rigidbody";
 
 /**
  * A behavior to move a GameObject based directly on it's transform position.
@@ -14,22 +15,24 @@ export class MovableLogicBehavior extends LogicBehavior<Vector3> {
   }
   /**
    * Set the translation speed of the GameObject (units per second). WIll be used to move the GameObject in the next update.
-   * @param translationSpeed - The translation speed to use.
+   * @param translationForce - The translation speed to use.
    */
-  public set translationSpeed(translationSpeed: Vector3) {
-    this.data = translationSpeed;
+  public set translationForce(translationForce: Vector3) {
+    this.data = translationForce;
     this.notifyDataChanged();
   }
 
   override tick(_deltaTime: number) {
     super.tick(_deltaTime);
-    this.gameObject.transform.position.add(this.data.clone().scale(_deltaTime));
+    this.gameObject
+      .getFirstBehavior(Rigidbody)
+      ?.addForce(this.data.clone().toVector2());
   }
 
   /**
    * Get the translation speed of the GameObject (units per second).
    */
-  public get translationSpeed(): Vector3 {
+  public get translationForce(): Vector3 {
     return this.data;
   }
 }
