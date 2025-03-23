78df75ed69c09476abfa4efdc9b0a9dfbb12f6d6
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2025-03-23 16:45:22 +0100

    test: add a test case for collision reaction between rigidbody and collider with full restitution

--- test/Extensions/PhysicsEngine/RigidBodies/Rigidbody.test.ts
+++ test/Extensions/PhysicsEngine/RigidBodies/Rigidbody.test.ts
@@ -27,9 +27,9 @@ describe("Rigidbody", (): void => {
 
     const vertices1: Vector2[] = [
       new Vector2(1, 1),
-      new Vector2(0, -1),
-      new Vector2(-1, -2),
-      new Vector2(0, 1),
+      new Vector2(1, -1),
+      new Vector2(-1, -1),
+      new Vector2(-1, 1),
     ];
     const polygonCollider1: PolygonCollider = new PolygonCollider(vertices1);
     const rigidBody1 = new Rigidbody(polygonCollider1, 1);
@@ -60,5 +60,54 @@ describe("Rigidbody", (): void => {
     expect(object1.transform.position.y).toBeCloseTo(44.145);
     expect(object1.transform.position.x).toBe(0);
     expect(rigidBody1.linearVelocity == new Vector2(0, 29.43));
+  });
+
+  /**
+   * Tests if the bounce on a collider works well with full restitution
+   */
+  it("should bounce on collider with full resitution", () => {
+    // Rigidbody
+    const object1: GameObject = new GameObject("Object1");
+    gameEngineWindow.root.addChild(object1);
+    const vertices1: Vector2[] = [
+      new Vector2(1, 0),
+      new Vector2(1, -2),
+      new Vector2(-1, -2),
+      new Vector2(-1, 0),
+    ];
+    const polygonCollider1: PolygonCollider = new PolygonCollider(vertices1);
+    const rigidBody1 = new Rigidbody(polygonCollider1, 1, 1);
+    object1.addBehavior(polygonCollider1);
+    object1.addBehavior(rigidBody1);
+
+    // Collider
+    const object2: GameObject = new GameObject("Object2");
+    gameEngineWindow.root.addChild(object2);
+    const vertices2: Vector2[] = [new Vector2(-1, 0), new Vector2(1, 0)];
+    const polygonCollider2: PolygonCollider = new PolygonCollider(vertices2);
+    object2.addBehavior(polygonCollider2);
+    object2.transform.position.set(0, 4.905, 0);
+
+    gameEngineWindow.addGameComponent(physicsGameEngineComponent);
+    // check position at s0
+    expect(object2.transform.position.y).toBe(4.905);
+    expect(object2.transform.position.x).toBe(0);
+    expect(object1.transform.position.y).toBe(0);
+    expect(object1.transform.position.x).toBe(0);
+    expect(rigidBody1.linearVelocity).toStrictEqual(new Vector2(0, 0));
+
+    // Check position at s1
+    manualTicker.tick(1);
+    expect(rigidBody1.linearVelocity.x).toBe(0);
+    expect(rigidBody1.linearVelocity.y).toBeCloseTo(-9.81);
+    expect(object1.transform.position.y).toBeCloseTo(4.905);
+    expect(object1.transform.position.x).toBe(0);
+
+    // Check position at s1
+    manualTicker.tick(1);
+    expect(rigidBody1.linearVelocity.x).toBe(0);
+    expect(rigidBody1.linearVelocity.y).toBeCloseTo(0);
+    expect(object1.transform.position.y).toBeCloseTo(0);
+    expect(object1.transform.position.x).toBe(0);
   });
 });
