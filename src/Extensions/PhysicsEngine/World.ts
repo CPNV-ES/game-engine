35c6d785e3a18904346a795ff5064b9ba70588da
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2025-03-25 23:45:32 +0100

    feat: remove the useless World class in physics engine

--- src/Extensions/PhysicsEngine/World.ts
+++
@@ -1,9 +0,0 @@
-import { Vector2 } from "@core/MathStructures/Vector2.ts";
-
-export class World {
-  public gravity: Vector2;
-
-  constructor(gravity: Vector2, friction: number) {
-    this.gravity = gravity;
-  }
-}
